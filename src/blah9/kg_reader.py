956daa785ac468edf193def75510b3f9179a88dd
Andrea Franchini <francan@usi.ch>
2025-01-16 15:44:37 +0900

    More scaffoldings

---
+++ src/blah9/kg_reader.py
@@ -0,0 +1,9 @@
+class KgReader:
+    def __init__(self) -> None:
+        # Get neo4j instance
+        ...
+    
+    def query_graph(self,):
+        # Provide system prompt, user query and graph schema repr to LLM
+        # Get back list of entities
+        
